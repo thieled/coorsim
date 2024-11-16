b384e3206c6d3e09f69fea21581f7e043998bc97
thieled <daniel.thiele@fu-berlin.de>
2024-11-17 00:45:47 +0100

    changes in get_embeddings

--- R/get_embeddings.R
+++ R/get_embeddings.R
@@ -2,21 +2,20 @@
 #'
 #' @param text_data A data.frame with 'id' and 'text' columns.
 #' @param model_name The Hugging Face model to use for embeddings (default is "twitter/twhin-bert-base").
-#' @param python_path Optional. The path to the Python executable to be used by reticulate.
 #' @return A data.frame with 'id' and the computed embeddings as additional columns.
 #' @export
-get_embeddings <- function(text_data, model_name = "twitter/twhin-bert-base", python_path = NULL) {
-  # Set Python path if specified
-  if (!is.null(python_path)) {
-    reticulate::use_python(python = python_path, required = TRUE)
+get_embeddings <- function(text_data, model_name = "twitter/twhin-bert-base") {
+  
+  
+  # Check if transformers is initialized
+  if(!options("transformers_initialized")$transformers_initialized){
+    init_transformers()
   }
   
-  #### NOTE: Improve handling of venv
-  
-  # Check Python environment availability
-  if (!reticulate::py_available()) {
-    stop("Python environment not available. Ensure Python is installed and configured correctly.")
+  if(!options("transformers_initialized")$transformers_initialized){
+    stop("Initializing transformers failed. Try 'install_transformers()'.")
   }
+  
   
   # Source py script
   reticulate::source_python(system.file("python",
@@ -26,18 +25,13 @@ get_embeddings <- function(text_data, model_name = "twitter/twhin-bert-base", py
   ))
   
   
-  # Convert the text column to a list
-  texts <- text_data$text
+  embeddings <- get_text_embeddings(texts = text_data$text, model_name = model_name)
   
-  # Call the Python function with model_name
-  embeddings <- get_text_embeddings(texts, model_name = model_name)
+  embeddings_m <- Matrix::as.matrix(embeddings)
   
-  # Convert numpy array to R data frame
-  embeddings_df <- as.data.frame(reticulate::py_to_r(embeddings))
+  rownames(embeddings_m) <- text_data$id
   
-  # Combine embeddings with IDs into a single data frame
-  embeddings_df <- cbind(id = text_data$id, embeddings_df)
-  
-  return(embeddings_df)
+  return(embeddings_m)
 }
+
 
